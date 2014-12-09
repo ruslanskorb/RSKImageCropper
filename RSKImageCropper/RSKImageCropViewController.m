46e413b7a783b228ddaa90ce3e53f9dae033c50f
Bastian Bense <bb@neo.de>
2014-12-09 12:47:30 +0100

    Top padding for label reduced

--- RSKImageCropper/RSKImageCropViewController.m
+++ RSKImageCropper/RSKImageCropViewController.m
@@ -29,7 +29,7 @@
 
 static const CGFloat kPortraitCircleMaskRectInnerEdgeInset = 15.0f;
 static const CGFloat kPortraitSquareMaskRectInnerEdgeInset = 20.0f;
-static const CGFloat kPortraitMoveAndScaleLabelVerticalMargin = 64.0f;
+static const CGFloat kPortraitMoveAndScaleLabelVerticalMargin = 20.0f;
 static const CGFloat kPortraitCancelAndChooseButtonsHorizontalMargin = 13.0f;
 static const CGFloat kPortraitCancelAndChooseButtonsVerticalMargin = 21.0f;
 
