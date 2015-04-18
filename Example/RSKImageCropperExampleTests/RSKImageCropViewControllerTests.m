ed7de8b08e7c2400b234826a54aaa84743754e83
Ruslan Skorb <ruslan.skorb@gmail.com>
2015-04-18 20:28:45 +0300

    [Image Crop View Controller] Add tests for reset.

--- Example/RSKImageCropperExampleTests/RSKImageCropViewControllerTests.m
+++ Example/RSKImageCropperExampleTests/RSKImageCropViewControllerTests.m
@@ -7,6 +7,21 @@
 //
 
 #import "RSKImageCropViewController.h"
+#import "RSKImageScrollView.h"
+
+@interface RSKImageCropViewController (Testing)
+
+@property (strong, nonatomic) RSKImageScrollView *imageScrollView;
+@property (assign, nonatomic) CGFloat rotationAngle;
+
+- (void)displayImage;
+- (void)reset:(BOOL)animated;
+- (void)resetContentOffset;
+- (void)resetFrame;
+- (void)resetRotation;
+- (void)resetZoomScale;
+
+@end
 
 SpecBegin(RSKImageCropViewController)
 
@@ -52,6 +67,87 @@ describe(@"initWithImage:cropMode:", ^{
     it(@"should init with specified crop mode", ^{
         RSKImageCropViewController *imageCropViewController = [[RSKImageCropViewController alloc] initWithImage:nil cropMode:RSKImageCropModeSquare];
         expect(imageCropViewController.cropMode).to.equal(RSKImageCropModeSquare);
+    });
+});
+
+describe(@"reset", ^{
+    __block RSKImageCropViewController *imageCropViewController = nil;
+    
+    before(^{
+        imageCropViewController = [[RSKImageCropViewController alloc] initWithImage:[UIImage imageNamed:@"photo"]];
+        // Loads view and calls viewDidLoad.
+        [imageCropViewController view];
+        
+        [imageCropViewController.view setNeedsUpdateConstraints];
+        [imageCropViewController.view updateConstraintsIfNeeded];
+        
+        [imageCropViewController.view setNeedsLayout];
+        [imageCropViewController.view layoutIfNeeded];
+    });
+    
+    it(@"should reset rotation", ^{
+        CGFloat initialRotationAngle = imageCropViewController.rotationAngle;
+        CGFloat testRotationAngle = M_PI_2;
+        imageCropViewController.rotationAngle = testRotationAngle;
+        [imageCropViewController resetRotation];
+        expect(imageCropViewController.rotationAngle).to.equal(initialRotationAngle);
+    });
+    
+    it(@"should reset frame", ^{
+        CGRect initialFrame = imageCropViewController.imageScrollView.frame;
+        CGRect testFrame = CGRectOffset(imageCropViewController.maskRect, 100, 100);
+        imageCropViewController.imageScrollView.frame = testFrame;
+        [imageCropViewController resetFrame];
+        expect(imageCropViewController.imageScrollView.frame).to.equal(initialFrame);
+    });
+    
+    it(@"should reset zoom scale", ^{
+        CGFloat initialZoomScale = imageCropViewController.zoomScale;
+        CGFloat testZoomScale = initialZoomScale + 0.1;
+        imageCropViewController.imageScrollView.zoomScale = testZoomScale;
+        [imageCropViewController resetZoomScale];
+        expect(imageCropViewController.zoomScale).to.equal(initialZoomScale);
+    });
+    
+    it(@"should reset content offset", ^{
+        CGPoint initialContentOffset = imageCropViewController.imageScrollView.contentOffset;
+        CGPoint testContentOffset = CGPointMake(initialContentOffset.x + 50, initialContentOffset.y + 50);
+        imageCropViewController.imageScrollView.contentOffset = testContentOffset;
+        [imageCropViewController resetContentOffset];
+        expect(imageCropViewController.imageScrollView.contentOffset).to.equal(initialContentOffset);
+    });
+    
+    it(@"should reset rotation, frame, zoom scale, content offset", ^{
+        CGFloat initialRotationAngle = imageCropViewController.rotationAngle;
+        CGRect initialFrame = imageCropViewController.imageScrollView.frame;
+        CGFloat initialZoomScale = imageCropViewController.zoomScale;
+        CGPoint initialContentOffset = imageCropViewController.imageScrollView.contentOffset;
+        
+        CGFloat testRotationAngle = M_PI_2;
+        CGRect testFrame = CGRectOffset(imageCropViewController.maskRect, 100, 100);
+        CGFloat testZoomScale = initialZoomScale + 0.1;
+        CGPoint testContentOffset = CGPointMake(initialContentOffset.x + 50, initialContentOffset.y + 50);
+        
+        imageCropViewController.rotationAngle = testRotationAngle;
+        
+        CGAffineTransform transform = imageCropViewController.imageScrollView.transform;
+        imageCropViewController.imageScrollView.transform = CGAffineTransformIdentity;
+        imageCropViewController.imageScrollView.frame = testFrame;
+        imageCropViewController.imageScrollView.transform = transform;
+        
+        imageCropViewController.imageScrollView.zoomScale = testZoomScale;
+        imageCropViewController.imageScrollView.contentOffset = testContentOffset;
+        
+        [imageCropViewController reset:NO];
+        
+        expect(imageCropViewController.rotationAngle).to.equal(initialRotationAngle);
+        expect(imageCropViewController.imageScrollView.frame).to.equal(initialFrame);
+        expect(imageCropViewController.zoomScale).to.equal(initialZoomScale);
+        expect(imageCropViewController.imageScrollView.contentOffset).to.equal(initialContentOffset);
+    });
+    
+    after(^{
+        imageCropViewController = nil;
     });
 });
 
