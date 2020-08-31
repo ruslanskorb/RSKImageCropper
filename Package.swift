7bcf292f6f8861dfb36da4fcba28875dfebdf3fe
Anton Savelev <savelev@worki.ru>
2020-08-31 15:33:44 +0300

    Fix package file

--- Package.swift
+++ Package.swift
@@ -20,6 +20,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "RSKImageCropper",
-            dependencies: [])
+            dependencies: [],
+            path: "RSKImageCropper")
     ]
 )
