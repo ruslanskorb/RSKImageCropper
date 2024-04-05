7978c97b8d3096453d24547961c55ffa81dd6ba4
Ruslan Skorb <ruslan@rsk-lab.com>
2024-04-05 20:50:40 +0300

    Revert "[RSKImageCropper] Add `RSKImageCropperResources.bundle` with `PrivacyInfo.xcprivacy`."
    This reverts commit 52b62311f1d678f944161d7ac957ce292b9cb687.

--- Package.swift
+++ Package.swift
@@ -16,7 +16,6 @@ let package = Package(
             name: "RSKImageCropper",
             path: "RSKImageCropper",
             resources: [
-                .copy("RSKImageCropperResources.bundle"),
                 .copy("RSKImageCropperStrings.bundle")
             ],
             publicHeadersPath: "include"
